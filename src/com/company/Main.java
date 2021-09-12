10e1cd663d8a403ce7beb8c8cf5bdb14457c7855
jelenbol <jelenbol@gmail.com>
2021-09-12 23:17:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,76 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // short NumberOne = 5;
+        // short NumberTwo = 10;
+
+        // if (NumberOne >= NumberTwo) {
+        //    System.out.println("Number One is bigger than Number Two");
+
+        // }
+
+        //    else
+
+        //    {
+        //      System.out.println("Number Two is bigger than Number One");
+
+
+        //   }
+
+
+        //short NumberOne = 20;
+        //  short NumberTwo = 20;
+
+        //  if (NumberOne == NumberTwo)
+
+        // {
+        //     System.out.println("Number One equals Number Two");
+        //  }
+
+        //      else {
+        //      System.out.println("Number One is not equal Number Two");
+        //}
+
+
+        // short NumberOne = 7;
+        // short NumberTwo = 8;
+
+        // if (NumberOne % 2 == 0) {
+        //     System.out.println("Number One is even");
+        //  }
+
+        //  else {
+        //  System.out.println("Number Two is even");
+        //  }
+
+
+        // short NumberOne = -5;
+        // short NumberTwo = 5;
+
+        //if (NumberOne <0)
+        // {
+        //   System.out.println("Number One is negative");}
+        // else {
+        //      System.out.println("Number Two is negative");
+        // }
+
+
+        // short NumberOne = 101;
+        //  short NumberTwo = 98;
+
+        //    if (NumberOne > 100 && NumberTwo > 100) {
+        //         System.out.println("Both Number One and Number Two are bigger than 100");
+        //    }
+
+        //     else
+
+        //        System.out.println("There is number less than 100");
+
+
+    }
+
+}
